75bc58a8605b20ac085e9042822034995de59d08
Vamshi Prasad <vamvijkum@gmail.com>
2020-10-22 01:21:53 +0530

    Display tasks on frontEnd

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,20 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
+    constructor() public {
+        createTask("First Task");
+    }
+
     uint256 public taskCount = 0;
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+    mapping(uint256 => Task) public tasks;
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
